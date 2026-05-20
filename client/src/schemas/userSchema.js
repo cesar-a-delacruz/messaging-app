e711783bce0a2c4869370d13a61aff2277068b4f
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-05-20 18:37:40 -0500

    feat(client): add userSchema for form fields

---
+++ client/src/schemas/userSchema.js
@@ -0,0 +1,39 @@
+export const allFields = [
+  {
+    id: "id",
+    value: "",
+    type: "hidden",
+  },
+  {
+    id: "username",
+    value: "",
+    label: "Username",
+    type: "text",
+  },
+  {
+    id: "fullname",
+    value: "",
+    label: "Fullname",
+    type: "text",
+  },
+  {
+    id: "password",
+    value: "",
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: "bio",
+    value: "",
+    label: "Bio",
+    type: "textarea",
+  },
+  {
+    id: "image",
+    value: "",
+    label: "Image",
+    type: "file",
+  },
+];
+
+export const loginFields = [allFields[1], allFields[3]];
