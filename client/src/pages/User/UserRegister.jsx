000fa433dbda4917b5af3f5456be73958d534699
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-05-22 16:29:26 -0500

    feat(client): add Form to UserRegister page

--- client/src/pages/User/UserRegister.jsx
+++ client/src/pages/User/UserRegister.jsx
@@ -1,7 +1,24 @@
+import requestHandler from "@/handlers/requestHandler";
+import { registerFields } from "@/schemas/userSchema";
+import Form from "@/components/Form";
+
 export default function UserRegister() {
   return (
     <div className="page">
       <h2>Register</h2>
+      <Form
+        fields={registerFields}
+        submit={{ text: "Create Account", handler: submitHandler }}
+      />
     </div>
   );
+
+  async function submitHandler(userData) {
+    if (userData.password !== userData.confirm)
+      return alert("The passwords don't match");
+
+    const register = await requestHandler.postFile(userData, "user");
+    if (register.error) return alert(register.error);
+    location.replace("/login");
+  }
 }
