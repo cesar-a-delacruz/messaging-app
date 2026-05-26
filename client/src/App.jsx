e5965240bcac7bb94b909132456b75a6f284af8d
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-05-26 03:18:40 -0500

    refactor(client): remove group code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import UserRoutes from "./routes/UserRoutes";
-import GroupRoutes from "./routes/GroupRoutes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <UserRoutes />
-      <GroupRoutes />
     </BrowserRouter>
   </StrictMode>,
 );
