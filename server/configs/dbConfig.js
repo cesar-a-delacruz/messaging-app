d65a0d43de61a4470bf7522453454c9f2e42c931
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-07-24 14:18:10 -0500

    fix(db): prevent user password from being exposed

--- server/configs/dbConfig.js
+++ server/configs/dbConfig.js
@@ -4,4 +4,5 @@ require("dotenv").config();
 
 module.exports = new PrismaClient({
   adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
+  omit: { user: { password: true } },
 });
