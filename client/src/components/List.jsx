f3f7a9365ce5ab381751c872d8facefcf1f87a05
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-07-24 08:00:36 -0500

    refactor(client): replace Chatlist with List

---
+++ client/src/components/List.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import styles from "./styles/List.module.css";
+
+export default function List({ items, clickHandler }) {
+  const [list, setList] = useState(items);
+
+  useEffect(() => {
+    setList(items);
+  }, [items]);
+
+  return (
+    <div className={styles.list}>
+      {list.map((item) => (
+        <div
+          key={item.id}
+          onClick={() => clickHandler(item)}
+          className={styles.item}
+        >
+          <img src={item.image} alt={`${item.title} picture`} />
+          <div className={styles.text}>
+            <h3>{item.title}</h3>
+            {item.content && <div>{item.content}</div>}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
