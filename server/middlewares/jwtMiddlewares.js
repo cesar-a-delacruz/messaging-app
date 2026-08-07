4cb68577d1df7907c93392be93b9efbb8b689aa4
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-08-07 14:54:05 -0500

    refactor(server): use user repository in jwtMiddlewares

--- server/middlewares/jwtMiddlewares.js
+++ server/middlewares/jwtMiddlewares.js
@@ -1,52 +1,40 @@
 const jwt = require("jsonwebtoken");
 const { compare } = require("bcryptjs");
 const dbConfig = require("../configs/dbConfig.js");
+const userRepository = require("../repositories/index.js").user;
 require("dotenv").config();
 
 module.exports = {
   authenticate: async (req, res) => {
-    try {
-      const { username, password } = req.body;
+    const { username, password } = req.body;
 
-      const user = await dbConfig.user.findUnique({
-        where: {
-          username,
-        },
-        select: {
-          id: true,
-          password: true,
-        },
-      });
-      if (user === null)
-        return res
-          .status(400)
-          .json({
-            error: "Credentials mismatch. Incorrect username.",
-          })
-          .end();
+    const user = await userRepository.findOneByUsername(username);
+    if (user === null)
+      return res
+        .status(400)
+        .json({
+          error: "Credentials mismatch. Incorrect username.",
+        })
+        .end();
 
-      const match = await compare(password, user.password);
-      if (!match)
-        return res
-          .status(400)
-          .json({
-            error: "Credentials mismatch. Incorrect password.",
-          })
-          .end();
+    const match = await compare(password, user.password);
+    if (!match)
+      return res
+        .status(400)
+        .json({
+          error: "Credentials mismatch. Incorrect password.",
+        })
+        .end();
 
-      const token = jwt.sign(
-        {
-          id: user.id,
-        },
-        process.env.JWT_SECRET,
-        { expiresIn: "1h" },
-      );
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
 
-      return res.status(200).json({ token }).end();
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: "Failed to find user." }).end();
-    }
+    return res.status(200).json({ token }).end();
   },
   authorize: async (req, res, next) => {
     const authHeader = req.headers["authorization"];
@@ -60,9 +48,7 @@ module.exports = {
     });
   },
   refresh: async (req, res) => {
-    const user = await dbConfig.user.findUnique({
-      where: { id: req.params.id },
-    });
+    const user = await userRepository.findOne(req.params.id);
     if (!user) return res.sendStatus(401);
 
     const payload = { id: user.id };
