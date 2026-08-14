580afd4609b2b57bbf8cc176ac14eeb4270ef57d
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-08-14 08:02:17 -0500

    fix(ui): make From fieldsets wrap

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import "./Form.module.css";
+import styles from "./Form.module.css";
 import { useRef, useState } from "react";
 import FormField from "../FormField/FormField";
 
@@ -17,19 +17,21 @@ export default function Form({ fieldsets, submit = { text, handler } }) {
 
   return (
     <form onSubmit={submitHandler} ref={form}>
-      {fieldsets.map((fieldset) => (
-        <fieldset key={fieldset.legend || ""}>
-          {fieldset.legend && <legend>{fieldset.legend}</legend>}
-          {fieldset.fields.map((field) => (
-            <FormField
-              properties={field}
-              value={data[field.id]}
-              changeHandler={changeHandler}
-              key={field.id}
-            />
-          ))}
-        </fieldset>
-      ))}
+      <div className={styles.fieldsets}>
+        {fieldsets.map((fieldset) => (
+          <fieldset key={fieldset.legend || ""}>
+            {fieldset.legend && <legend>{fieldset.legend}</legend>}
+            {fieldset.fields.map((field) => (
+              <FormField
+                properties={field}
+                value={data[field.id]}
+                changeHandler={changeHandler}
+                key={field.id}
+              />
+            ))}
+          </fieldset>
+        ))}
+      </div>
       <button type="submit">{submit.text}</button>
     </form>
   );
