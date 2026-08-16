f1493220b5f7b52e806440108380d6ff8c7f849d
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2026-08-15 21:01:06 -0500

    fix(server): pass user payload in refreshToken

--- server/services/AuthService.js
+++ server/services/AuthService.js
@@ -6,7 +6,9 @@ module.exports = {
     const match = await compare(password, user.password);
     if (!match) return null;
 
-    return jwt.sign({}, process.env.JWT_SECRET, { expiresIn: "7d" });
+    return jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
   },
   access: (userId) =>
     jwt.sign(
